08aa55e0c412ada6654f31005e680952163c5342
Franklin Jara <franklinjaradev@gmail.com>
2023-01-16 21:03:54 -0500

    :sparkles: style: style udpdate for dashboard and settings

--- src/pages/settings.tsx
+++ src/pages/settings.tsx
@@ -15,8 +15,8 @@ const Settings = () => {
           <p className="custom-p">Your names: Franklin & Sydney</p>
           <p className="custom-p">Together since: Aug 14, 2020</p>
           <h2 className="custom-h2 text-rose-600 font-semibold">Picture</h2>
-          <button className="custom-button text-lg">Choose a custom picture</button>
-          <button className="custom-button text-lg">Choose from default pictures</button>
+          <button className="custom-button text-base md:text-lg">Choose a custom picture</button>
+          <button className="custom-button text-base md:text-lg">Choose from default pictures</button>
           <h2 className="custom-h2 text-rose-600 font-semibold">Notifications</h2>
           <div className="flex items-center gap-2">
             <p className="custom-p">Notify me on special days</p>
